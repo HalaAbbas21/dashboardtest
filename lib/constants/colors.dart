a22a05f394e296282f49ecad7dcb501b3046ee18
HalaAbbas21 <119543956+HalaAbbas21@users.noreply.github.com>
2023-05-06 17:59:38 +0300

    Add files via upload

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+class AppColors{
+  static const primary=Color(0xFF78A6C8); //for button and lines in secondary background
+  static const secondary =Color(0xFFE9EEF2); // background and the color of word in primary buttons
+ static const primaryDark=Color(0xFFE9EEF2); //just for now
+ static const secondaryDark=Color(0xFF05141F);//for background in dark
+}
